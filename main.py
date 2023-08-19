98046714cfb56d2c5cc73877a8663cca2215e584
Jack Murray <104339627+LHJMurr@users.noreply.github.com>
2023-08-18 17:15:20 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         baudRate = int(input("Enter baud rate: "))
     except:
         raise Exception("Inputted baud rate must be an integer.")
-    ser = serial.Serial(portStr, baudRate)
+    ser = serial.Serial(portStr, baudRate, timeout = 1.0)
     ser.reset_input_buffer() # Clear incoming/outgoing signals
     ser.reset_output_buffer()
     time.sleep(3)
@@ -21,15 +21,16 @@ if __name__ == '__main__':
             if (ser.in_waiting > 0):
                 # Read serial information
                 line = ser.readline()
-                potStr = line.decode('utf-8').strip()
-                print(repr(potStr))
+                potStr = line.decode('utf-16').strip()
                 potVal = int(potStr)
+                print(f'REPR of decoded string: {repr(potStr)}   ||   Integer Value detected: {potVal}')
                 # Publish LED state
                 if (potVal > 500):
                     LEDState = 1
                 else:
                     LEDState = 0
                 sendStr = str(LEDState) + '\n'
+                print(f'String to send: {repr(sendStr)}')
                 ser.write(sendStr.encode('utf-8'))
     except Exception as e:
         print("Exception raised. Closing serial communications...")
